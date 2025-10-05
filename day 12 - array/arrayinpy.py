a17ac75243d2c8149ab4c80b114fae5b1c4e95f7
Kushu-Shukla <kushushukla24@gmail.com>
2025-10-05 14:55:00 +0530

    happy

--- day 12 - array/arrayinpy.py
+++ day 12 - array/arrayinpy.py
@@ -1,11 +1,11 @@
 # !. Basics of Array
 # Quese -> What is an Array
 # Ans -> An array is a collection of iterms stored at contiguous memory location of same type. The idea is to store the multuple items of the same type together. This make it easier to calulate the position of each element by simply adding an offset to a base value, i.e the memory location of the first element of the array
-age_of_a = 22
+age_of_a = -22
 age_of_k = 150
 age_of_r = 24
 
-print(id(age_of_a))
+print(age_of_a)
 print(id(age_of_k))
 print(id(age_of_r))
 
@@ -15,7 +15,8 @@ arr = array('i', [1,2,3,4,5])
 print(id(arr))
 print(type(arr))
 
-arr2 = array("B")
+arr2 = array("I", [23,53,32,35,1,2,535])
+print(arr2)
 
 # here to define an array of a particular type we need to use type-code, just like in the above case we used 'i' for integer type array. 
 # Some commonly used type codes are:
@@ -36,3 +37,14 @@ arr2 = array("B")
 # 'c' - char (string of length 1)
 # 'P' - pointer (used for storing memory addresses)
 # '?' - boolean (True or False)
+
+# signed integer -> -2,147,483,648 to 2,147,483,64
+var = -1923147483648
+print(var)
+print(type(var))
+# unsigned integer -> 0 to 4,294,967,28
+
+Ques -> Static Array vs Dynamic Array
+Ans -> 
+
+Static Array
