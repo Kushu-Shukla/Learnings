2750c560a6b70751a38da2a9798cef8fa5116c23
Kushu-Shukla <kushushukla24@gmail.com>
2025-10-05 14:45:31 +0530

    sdf

--- day 12 - array/arrayinpy.py
+++ day 12 - array/arrayinpy.py
@@ -1,12 +1,19 @@
-# Array -> array is a data structure that stores a fixed size collection of elements of the same data type, all located in contigous memory location.
+# !. Basics of Array
+# Quese -> What is an Array
+# Ans -> An array is a collection of iterms stored at contiguous memory location of same type. The idea is to store the multuple items of the same type together. This make it easier to calulate the position of each element by simply adding an offset to a base value, i.e the memory location of the first element of the array
+age_of_a = 22
+age_of_k = 150
+age_of_r = 24
 
-import array
+print(id(age_of_a))
+print(id(age_of_k))
+print(id(age_of_r))
 
-nums = array.array('i', [10, 20, 30]) # i means integer type
-nums.append(40)
-print(nums * 2)
+from array import *
 
-import numpy as np
+arr = array('i', [1,2,3,4,5])
+print(id(arr))
+print(type(arr))
 
-arr = np.array([1,2,3,4])
-print(arr * 2)
+arr2 = array("B")
+
